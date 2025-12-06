e30be7bf286710778fb528ccde450a28950759c1
ClaytonTKC <Claytoncheung31@gmail.com>
2025-12-06 15:21:53 -0500

    Created the Business Entities

---
+++ webservice/src/main/java/com/envisionad/webservice/business/dataaccesslayer/CompanySize.java
@@ -0,0 +1,12 @@
+package com.envisionad.webservice.business.dataaccesslayer;
+
+public enum CompanySize {
+    SMALL("1 - 50"),
+    MEDIUM("51 - 150"),
+    LARGE("151 - 500"),
+    ENTERPRISE("500+")
+
+    private final String label;
+
+
+}
