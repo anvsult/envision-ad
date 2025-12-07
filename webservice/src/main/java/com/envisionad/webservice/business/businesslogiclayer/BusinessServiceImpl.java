88063865d9a80cc7a33f55ffddb92849271cb3df
ClaytonTKC <Claytoncheung31@gmail.com>
2025-12-06 21:20:53 -0500

    Created Business seervice

--- webservice/src/main/java/com/envisionad/webservice/business/businesslogiclayer/BusinessServiceImpl.java
+++ webservice/src/main/java/com/envisionad/webservice/business/businesslogiclayer/BusinessServiceImpl.java
@@ -1,4 +1,37 @@
 package com.envisionad.webservice.business.businesslogiclayer;
 
-public class BusinessServiceImpl {
+import com.envisionad.webservice.business.dataaccesslayer.Business;
+import com.envisionad.webservice.business.dataaccesslayer.BusinessRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class BusinessServiceImpl implements BusinessService {
+
+    private final BusinessRepository businessRepository;
+
+    public BusinessServiceImpl(BusinessRepository businessRepository) {
+        this.businessRepository = businessRepository;
+    }
+
+    @Override
+    public Business createBusiness(Business business) {
+        if (businessRepository.existsByName(business.getName())) {
+            throw new RuntimeException("Business with name " + business.getName() + " already exists");
+        }
+        return businessRepository.save(business);
+    }
+
+    @Override
+    public List<Business> getAllBusinesses() {
+        return businessRepository.findAll();
+    }
+
+    @Override
+    public Business getBusinessById(UUID id) {
+        return businessRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Business not found with id: " + id));
+    }
 }
