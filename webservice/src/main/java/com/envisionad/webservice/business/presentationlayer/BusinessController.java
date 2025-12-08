d6264b2068ab4182e3320082ecd24e475aba0552
Anvar Sultanov <anv.sult@gmail.com>
2025-12-08 03:00:39 -0500

    Added Update and Delete methods to Businesses

--- webservice/src/main/java/com/envisionad/webservice/business/presentationlayer/BusinessController.java
+++ webservice/src/main/java/com/envisionad/webservice/business/presentationlayer/BusinessController.java
@@ -14,7 +14,7 @@ import java.util.UUID;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/api/v1/business")
+@RequestMapping("/api/v1/businesses")
 public class BusinessController {
 
     private final BusinessService businessService;
@@ -49,6 +49,25 @@ public class BusinessController {
     public ResponseEntity<BusinessResponseModel> getBusinessById(@PathVariable UUID id) {
         Business business = businessService.getBusinessById(id);
         BusinessResponseModel responseModel = businessResponseMapper.entityToResponseModel(business);
+
+        return ResponseEntity.ok(responseModel);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<BusinessResponseModel> updateBusinessById(
+            @PathVariable UUID id,
+            @RequestBody BusinessRequestModel requestModel) {
+        Business businessEntity = businessResponseMapper.requestModelToEntity(requestModel);
+        Business updatedBusiness = businessService.updateBusinessById(id, businessEntity);
+        BusinessResponseModel responseModel = businessResponseMapper.entityToResponseModel(updatedBusiness);
+
+        return ResponseEntity.ok(responseModel);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<BusinessResponseModel> deleteBusinessById(@PathVariable UUID id) {
+        Business deletedBusiness = businessService.deleteBusinessById(id);
+        BusinessResponseModel responseModel = businessResponseMapper.entityToResponseModel(deletedBusiness);
 
         return ResponseEntity.ok(responseModel);
     }
