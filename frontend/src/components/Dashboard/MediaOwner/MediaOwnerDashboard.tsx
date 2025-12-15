3e67479821a740871b603cfa49a385fe8b28a5dc
Pkdotts <andybaole1234@gmail.com>
2025-12-14 22:07:27 -0500

    fixed notification message

--- frontend/src/components/Dashboard/MediaOwner/MediaOwnerDashboard.tsx
+++ frontend/src/components/Dashboard/MediaOwner/MediaOwnerDashboard.tsx
@@ -36,14 +36,16 @@ export default function MediaOwnerPage() {
             if (editingId) {
                 await editMedia(editingId, formState);
                 notifications.show({
-                    title: t("success.update"),
+                    title: t("success.title"),
+                    message: t("success.update"),
                     color: "green",
                     icon: <IconCheck size="1.1rem" />,
                 });
             } else {
                 await addNewMedia(formState);
                 notifications.show({
-                    title: t("success.create"),
+                    title: t("success.title"),
+                    message: t("success.create"),
                     color: "green",
                     icon: <IconCheck size="1.1rem" />,
                 });
