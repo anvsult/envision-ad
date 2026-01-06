095e22093eccc00878329cccf9bd2734ae00ce28
Anvar Sultanov <anv.sult@gmail.com>
2026-01-06 17:44:35 -0500

    Updated ad-campaign features (functions) to use the shared axiosInstance

--- frontend/src/features/ad-campaign-management/api/addAdToCampaign.ts
+++ frontend/src/features/ad-campaign-management/api/addAdToCampaign.ts
@@ -1,25 +1,10 @@
 import {Ad, AdRequestDTO} from "@/entities/ad";
-import {getAccessToken} from "@auth0/nextjs-auth0";
-const API_BASE_URL = "http://localhost:8080/api/v1/ad-campaigns";
+import axiosInstance from "@/shared/api/axios/axios";
 
 export const addAdToCampaign = async (
     campaignId: string,
     data: AdRequestDTO
 ): Promise<Ad> => {
-    const token = await getAccessToken();
-    // We construct the URL: /api/v1/ad-campaigns/{campaignId}/ads
-    const res = await fetch(`${API_BASE_URL}/${campaignId}/ads`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${token}`
-        },
-        body: JSON.stringify(data),
-    });
-
-    if (!res.ok) {
-        throw new Error(`Failed to create ad: ${res.statusText}`);
-    }
-
-    return res.json();
+    const response = await axiosInstance.post(`/ad-campaigns/${campaignId}/ads`, data);
+    return response.data;
 };
--- frontend/src/features/ad-campaign-management/api/createAdCampaign.ts
+++ frontend/src/features/ad-campaign-management/api/createAdCampaign.ts
@@ -1,24 +1,9 @@
 import {AdCampaign, AdCampaignRequestDTO} from "@/entities/ad-campaign";
-import {getAccessToken} from "@auth0/nextjs-auth0";
-
-const API_BASE_URL = "http://localhost:8080/api/v1/ad-campaigns";
+import axiosInstance from "@/shared/api/axios/axios";
 
 export const createAdCampaign = async (
     data: AdCampaignRequestDTO
 ): Promise<AdCampaign> => {
-    const token = await getAccessToken();
-    const res = await fetch(API_BASE_URL, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${token}`
-        },
-        body: JSON.stringify(data),
-    });
-
-    if (!res.ok) {
-        throw new Error(`Failed to create ad campaign: ${res.statusText}`);
-    }
-
-    return res.json();
+    const response = await axiosInstance.post(`/ad-campaigns`, data);
+    return response.data;
 };
