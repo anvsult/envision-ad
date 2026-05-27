483f83769dc6ea372ecd9c67f1bbec187fbf1834
Anvar Sultanov <anv.sult@gmail.com>
2026-05-26 21:09:00 -0400

    Fixed api version for tests to connect to the docker database

--- webservice/src/test/java/com/envisionad/webservice/config/TestcontainersConfig.java
+++ webservice/src/test/java/com/envisionad/webservice/config/TestcontainersConfig.java
@@ -8,10 +8,13 @@ import org.testcontainers.containers.PostgreSQLContainer;
 @TestConfiguration(proxyBeanMethods = false)
 public class TestcontainersConfig {
 
-    private static final PostgreSQLContainer<?> postgres =
-            new PostgreSQLContainer<>("postgres:15");
+    private static final PostgreSQLContainer<?> postgres;
 
     static {
+        // Override Docker API version to satisfy OrbStack/Docker Engine 29.x (min API 1.40
+        // Testcontainers' shaded docker-java uses property key "api.version" (not DOCKER_API_VERSION
+        System.setProperty("api.version", "1.43");
+        postgres = new PostgreSQLContainer<>("postgres:15");
         postgres.start();
     }
 
